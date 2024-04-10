329f404f31ecc65298dfcbc60a17606c5a3bd9ba
Vaishnavi-Dubey <163510699+Vaishnavi-Dubey@users.noreply.github.com>
2024-04-10 23:35:48 +0530

    Create constants.c

---
+++ constants.c
@@ -0,0 +1,12 @@
+// constant variable
+#include <stdio.h>
+
+int main() {
+    // declaring a constant variable
+    const int var;
+    // initializing constant variable var after declaration
+    var = 20;
+
+    printf("Value of var: %d", var);
+    return 0;
+}
