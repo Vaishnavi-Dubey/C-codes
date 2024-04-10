2d4a1faa5174312f061804817a0e7cec01a85e2f
Vaishnavi-Dubey <163510699+Vaishnavi-Dubey@users.noreply.github.com>
2024-04-10 23:35:18 +0530

    Create constant.c

---
+++ constant.c
@@ -0,0 +1,21 @@
+// constant variable definition
+#include <stdio.h>
+
+int main() {
+
+    // defining integer constant using const keyword
+    const int int_const = 25;
+
+    const char char_const = 'A';
+
+    const float float_const = 15.66;
+
+    printf("Printing value of Integer Constant: %d\n",
+           int_const);
+    printf("Printing value of Character Constant: %c\n",
+           char_const);
+    printf("Printing value of Float Constant: %f",
+           float_const);
+
+    return 0;
+}
